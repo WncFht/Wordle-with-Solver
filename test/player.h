2c15c538a0d7b3889781daa3150412d2267e7dc5
WncFht <2130212584@qq.com>
2024-11-26 10:59:29 +0800

    Update release

--- test/player.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-
-#include "wordle.h"
-
-char* player_AI1(const char lastResult[WORD_LENGTH + 1]);
-char* player_AI2(const char lastResult[WORD_LENGTH + 1]);
-
-#endif
--- test_final/player.h
+++
@@ -1,8 +0,0 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-
-#include "wordle.h"
-
-char* player_AI(const char lastResult[WORD_LENGTH + 1]);
-
-#endif
